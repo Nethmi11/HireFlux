9f383dcfad6684af2c24aad1e37b8db104ca7fbf
Nethmi11 <nethmig03@gmail.com>
2024-06-06 12:00:00 +0530

    jobs route added

--- aid-backend/src/index.ts
+++ aid-backend/src/index.ts
@@ -3,4 +3,24 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-app.listen(8000, () => console.log("Server is listening on port 8000."));
+const jobs = [
+    {
+      _id: "xyz",
+      title: "Intern - Software Engineer",
+      type: "Full-time",
+      location: "Remote",
+    },
+    {
+      _id: "abc",
+      title: "Software Engineer",
+      type: "Full-time",
+      location: "Remote",
+    },
+  ];
+
+app.get("/jobs",(req, res) =>{
+    res.json(jobs)
+})
+
+
+app.listen(4000, () => console.log("Server is listening on port 4000."));
