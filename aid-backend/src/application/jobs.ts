eebe837a8018b7029eab18a2e2505c1b77854a3a
Nethmi11 <nethmig03@gmail.com>
2024-06-21 12:00:00 +0530

    connection to mongodb database

--- aid-backend/src/application/jobs.ts
+++ aid-backend/src/application/jobs.ts
@@ -1,46 +1,41 @@
 import { Request, Response } from "express";
 import jobs from "../infrastructure/jobs";
+import Job from "../infrastructure/schemas/job";
 
-export const getAllJobs = (req: Request, res: Response) => {
-    return res.json(jobs);
+export const getAllJobs = async (req: Request, res: Response) => {
+    const jobs = await Job.find();
+    return res.status(200).json(jobs);
 }
 
-export const createJob = (req: Request, res: Response) => {
-    
+export const createJob = async (req: Request, res: Response) => {
     const job = req.body;
-    
-    if (!(typeof job.title === "string" && typeof job._id === "string" && typeof job.location === "string" && typeof job.type === "string")) {
-        return res.status(400).send();
-    }
-    jobs.push(req.body);
+    await Job.create(job);
     return res.status(201).send();
 }
 
 
-export const getJobById = (req: Request, res: Response) => {
-    const job = jobs.find(el => el._id === req.params._id);
+export const getJobById = async (req: Request, res: Response) => {
+    const job = await Job.findById(req.params._id);
     if (!job) {
         return res.status(404).send();
     }
-    return res.json(job);
+    return res.status(200).json(job);
 }
 
-export const deleteJob = (req: Request, res: Response) => {
-    const indexToRemove = jobs.findIndex(el => el._id === req.params._id);
-    if (indexToRemove === -1) {
+export const deleteJob = async (req: Request, res: Response) => {
+    const job = await Job.findByIdAndDelete(req.params._id);
+    if (!job) {
         return res.status(404).send();
     }
-    jobs.splice(indexToRemove, 1);
     return res.status(204).send();
 }
 
-export const updateJob = (req: Request, res: Response) => {
-    const indexToUpdate = jobs.findIndex(el => el._id === req.params._id);
-    if (indexToUpdate === -1) {
+export const updateJob = async (req: Request, res: Response) => {
+    const jobToUpdate = Job.findById(req.params._id);
+    if (!jobToUpdate) {
         return res.status(404).send();
     }
-    jobs[indexToUpdate].title = req.body.title;
-    jobs[indexToUpdate].location = req.body.location;
-    jobs[indexToUpdate].type = req.body.type;
+
+    await Job.findByIdAndUpdate(req.params._id, { title: req.body.title, location: req.body.location, type: req.body.type });
     return res.status(204).send();
 }
