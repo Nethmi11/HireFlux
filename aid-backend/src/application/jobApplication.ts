caf59e79e37df1e40df99249a19a3e550f37b541
Nethmi11 <nethmig03@gmail.com>
2024-07-11 12:00:00 +0530

    added middleware/error handling

--- aid-backend/src/application/jobApplication.ts
+++ aid-backend/src/application/jobApplication.ts
@@ -6,11 +6,17 @@ export const createJobApplication = async (
   res: Response,
   next: NextFunction
 ) => {
-  const jobApplication = req.body;
-  console.log(jobApplication);
+  try {
+    const jobApplication = req.body;
+    console.log(jobApplication);
 
-  const createdJobApplication = await JobApplication.create(jobApplication);
-  return res.status(201).send();
+    const createdJobApplication = await JobApplication.create(jobApplication);
+    return res.status(201).send();
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send();
+  }
+
 };
 
 
@@ -18,16 +24,21 @@ export const getJobApplications = async (
   req: Request,
   res: Response,
 ) => {
+  try {
+    const { jobid } = req.query;
 
-  const { jobid } = req.query;
+    if (jobid) {
+      const jobApplications = await JobApplication.find({ job: jobid })
+      return res.status(200).json(jobApplications);
+    }
 
-  if (jobid) {
-    const jobApplications = await JobApplication.find({ job: jobid })
+    const jobApplications = await JobApplication.find().populate("job").exec();
     return res.status(200).json(jobApplications);
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send();
   }
 
-  const jobApplications = await JobApplication.find().populate("job").exec();
-  return res.status(200).json(jobApplications);
 
 };
 
@@ -35,10 +46,16 @@ export const getJobApplicationById = async (
   req: Request,
   res: Response,
 ) => {
-  const { id } = req.params;
-  const jobApplication = await JobApplication.findById(id);
-  if (jobApplication === null) {
-    return res.status(404).send();
+  try {
+    const { id } = req.params;
+    const jobApplication = await JobApplication.findById(id);
+    if (jobApplication === null) {
+      return res.status(404).send();
+    }
+    return res.status(200).json(jobApplication);
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send();
   }
-  return res.status(200).json(jobApplication);
+
 }
--- aid-backend/src/application/jobs.ts
+++ aid-backend/src/application/jobs.ts
@@ -1,41 +1,73 @@
-import { Request, Response } from "express";
-import jobs from "../infrastructure/jobs";
+import { NextFunction, Request, Response } from "express";
 import Job from "../infrastructure/schemas/job";
+import NotFoundError from "../domain/errors/not-found-error";
+import ValidationError from "../domain/errors/validation-error";
+import { z } from "zod";
 
-export const getAllJobs = async (req: Request, res: Response) => {
-    const jobs = await Job.find();
-    return res.status(200).json(jobs);
-}
-
-export const createJob = async (req: Request, res: Response) => {
-    const job = req.body;
-    await Job.create(job);
-    return res.status(201).send();
-}
-
-
-export const getJobById = async (req: Request, res: Response) => {
-    const job = await Job.findById(req.params._id);
-    if (!job) {
-        return res.status(404).send();
+export const getAllJobs = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const jobs = await Job.find();
+        return res.status(200).json(jobs);
+    } catch (error) {
+        next(error)
     }
-    return res.status(200).json(job);
 }
 
-export const deleteJob = async (req: Request, res: Response) => {
-    const job = await Job.findByIdAndDelete(req.params._id);
-    if (!job) {
-        return res.status(404).send();
+export const createJob = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const job = z.object({ title: z.string(), description: z.string(), type: z.string(), location: z.string(), questions: z.string().array().optional() }).safeParse(req.body)
+        if (!job.success) {
+            throw new ValidationError(job.error.message)
+        }
+
+        await Job.create(job);
+        return res.status(201).send();
+    } catch (error) {
+        next(error)
     }
-    return res.status(204).send();
 }
 
-export const updateJob = async (req: Request, res: Response) => {
-    const jobToUpdate = Job.findById(req.params._id);
-    if (!jobToUpdate) {
-        return res.status(404).send();
+
+export const getJobById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const job = await Job.findById(req.params._id);
+        if (!job) {
+            throw new NotFoundError("Job not found")
+        }
+        return res.status(200).json(job);
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const deleteJob = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+        const job = await Job.findByIdAndDelete(req.params._id);
+        if (!job) {
+            throw new NotFoundError("Job not found")
+        }
+        return res.status(204).send();
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const updateJob = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const jobToUpdate = Job.findById(req.params._id);
+        if (!jobToUpdate) {
+            throw new NotFoundError("Job not found")
+        }
+        const job = z.object({ title: z.string(), description: z.string(), type: z.string(), location: z.string(), questions: z.string().array().optional() }).safeParse(req.body)
+        if (!job.success) {
+            throw new ValidationError(job.error.message)
+        }
+        await Job.findByIdAndUpdate(req.params._id, { title: req.body.title, description: req.body.description, location: req.body.location, type: req.body.type });
+        return res.status(204).send();
+    } catch (error) {
+        next(error)
     }
 
-    await Job.findByIdAndUpdate(req.params._id, { title: req.body.title, location: req.body.location, type: req.body.type });
-    return res.status(204).send();
 }
