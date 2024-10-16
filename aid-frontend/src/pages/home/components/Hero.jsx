94eed084e37a70c7ca58feb149eb8363c4286cc2
Nethmi11 <127391055+Nethmi11@users.noreply.github.com>
2024-10-16 13:46:58 +0530

    Update Hero.jsx

--- aid-frontend/src/pages/home/components/Hero.jsx
+++ aid-frontend/src/pages/home/components/Hero.jsx
@@ -2,7 +2,7 @@ function Hero() {
   return (
     <section className="py-32 flex justify-center items-center rounded-xl shadow-sm shadow-gray-900 border-white hero">
       <div>
-        <h1 className="text-6xl font-bold text-white drop-shadow-[0_50px_50px_rgba(0,0,0,0.5)]">
+        <h1 className="text-6xl font-extrabold text-white drop-shadow-[0_10px_10px_rgba(0,0,0,0.8)]">
           Find your dream job
         </h1>
       </div>
