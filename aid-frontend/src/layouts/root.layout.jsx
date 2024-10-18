24ce75330a6202d32e844491f547cb9c7cf4110c
Nethmi11 <127391055+Nethmi11@users.noreply.github.com>
2024-10-18 19:39:11 +0530

    Update root.layout.jsx

--- aid-frontend/src/layouts/root.layout.jsx
+++ aid-frontend/src/layouts/root.layout.jsx
@@ -2,9 +2,12 @@ import { Outlet } from "react-router-dom";
 
 function RootLayout() {
   return (
-    <main className="container">      
-      <Outlet />
-    </main>
+    <div className="relative min-h-screen">
+      <div className="background-gradient"></div>
+      <main className="container mx-auto relative z-10 p-4">      
+        <Outlet />
+      </main>
+    <div/>
   );
 }
 
